1e0526c4d800c2c0dd130d1de3c2cce30618be1b
Chris Klassen <ca.klassen95@gmail.com>
2015-04-01 13:14:23 -0700

    Finished lab 8 (part 2) Signed-off-by:Chris Klassen <ca.klassen95@gmail.com>

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,19 +7,34 @@
  *
  * ------------------------------------------------------------------------
  */
-class Alpha extends Application {
+class Auth extends Application {
 
     function __construct() {
         parent::__construct();
+        $this->load->helper('url');
     }
 
-    //-------------------------------------------------------------
-    //  Stuff for everyone
-    //-------------------------------------------------------------
-
-    function index() {
-        $this->data['pagebody'] = 'alpha';
+    function index()
+    {
+        $this->data['pagebody'] = 'login';
         $this->render();
     }
 
+     function login() {
+        $key = $_POST['userid'];
+        $user = $this->users->get($key);
+        
+        if (password_verify($this->input->post('password'), $user->password)) {
+          $this->session->set_userdata('userID',$key);
+          $this->session->set_userdata('userName',$user->name);
+          $this->session->set_userdata('userRole',$user->role);
+        }
+        
+        redirect('/');
+    }
+    
+    function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
+    }
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,5 @@
-<div class="row">
-    <h2>Baby Steps</h2>
-
-    <p>An investor doesn't have a prayer of picking a manager that can deliver true alpha.</p>
-    <h4>Eugene Fama</h4>
-        
-</div>
+<form name="login" method="post" action="/auth/login">
+  UserID: <input type="text" name="userid"></input><br/>
+  Password: <input type="password" name="password"></input><br/>
+  <input type="submit">Submit</input>
+</form>
